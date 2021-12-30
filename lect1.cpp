ce7242bdeb740f4a23db82e862067b6b851becd3
Harsh dubey <95978233+mrharshpandit@users.noreply.github.com>
2021-12-30 10:06:09 +0530

    Add files via upload

---
+++ lect1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+//it is a hader file 
+
+using namespace std;
+int main()//starting point of program
+{//begning 
+   cout<<"code by harsh";
+   //syntax 
+   //cout<<"your message";
+   //cout means print
+}//end
