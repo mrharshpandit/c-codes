7573ef3343f7383db03821a4ab4814027382d7bc
Harsh dubey <95978233+mrharshpandit@users.noreply.github.com>
2022-01-04 15:47:45 +0530

    Add files via upload

---
+++ lect4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=5;
+	char b='h';
+	float c=12.09;
+	double d=13.09;
+	
+	cout<<"value of a is "<<a<<endl;
+	cout<<"value of b is "<<b<<endl;
+	cout<<"value of c is "<<c<<endl;
+	cout<<"value of d is "<<d<<endl;
+
+	
+}
